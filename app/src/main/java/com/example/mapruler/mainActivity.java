b64a960436baef377a110e7d51e4e52a8667f7e0
dood3 <dood345@gmail.com>
2025-11-04 21:56:12 -0600

    working first map

---
+++ app/src/main/java/com/example/mapruler/mainActivity.java
@@ -0,0 +1,19 @@
+package com.example.mapruler;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class mainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+
+        finish();
+    }
+}
